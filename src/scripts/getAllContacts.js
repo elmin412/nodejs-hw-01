e376bf2b6bfaf7312c50928b96e3f19204b1f7dc
elmin412 <115942958+elmin412@users.noreply.github.com>
2024-06-14 15:53:35 +0200

    fix
    get-all array

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -5,11 +5,13 @@ import fs from 'node:fs/promises';
 export const getAllContacts = async () => {
     try {
         const data = await fs.readFile(PATH_DB, 'utf-8');
-        return data;
+        const dbData = JSON.parse(data);
+        return dbData.contacts;
 
 
     } catch (err) {
         console.error('Error', err );
+        return [];
     }
 };
 
